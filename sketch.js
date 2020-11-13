268307421b33652622051a8f2f2a84d8cf1c8698
nathanramnath21 <71851194+nathanramnath21@users.noreply.github.com>
2020-11-13 11:54:58 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//Create variables here
+var dog, happyDog, database, foodS, foodStock;
+var dogImg, happyDogImage;
+function preload()
+{
+
+  dogImg = loadImage("images/dogImg.png");
+  happyDogImage = loadImage("images/dogImage1.png");
+
+}
+
+function setup() {
+	createCanvas(500, 500);
+  dog.createSprite(250,250,5,5);
+  dog.addImage(dogImg);
+}
+
+
+function draw() {  
+
+  drawSprites();
+
+}
+
+
+
