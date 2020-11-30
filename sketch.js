4c8aeed6928db793ed2b4a38cbb35d34751f5d37
shafhad <66770462+shafhad@users.noreply.github.com>
2020-11-30 16:22:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,13 +5,13 @@ function preload()
 {
 
   dogImg = loadImage("images/dogImg.png");
- // happyDogImage = loadImage("images/dogImg1.png");
+ happyDogImage = loadImage("images/dogImg1.png");
 
 }
 
 function setup() {
 	createCanvas(500, 500);
-  dog.createSprite(250,250,5,5);
+  dog=createSprite(250,250,5,5);
   dog.addImage(dogImg);
 }
 
